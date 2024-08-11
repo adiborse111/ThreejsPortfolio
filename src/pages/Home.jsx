c387167549c48272ea772ab9bc5f846ec5748fc3
adiborse111 <adi.borse111@gmail.com>
2024-08-11 19:53:35 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-0 flex items-center justify-center">
+        Popup
+      </div> */
+}
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-full bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+           
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
